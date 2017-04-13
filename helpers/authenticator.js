89fda8d0ccbff11d3ceb4f1c079d07d9dc2cc0cd
RedFing <muhamed.ahmatovic@gmail.com>
2017-04-13 10:59:06 +0200

    login complete

--- helpers/authenticator.js
+++ helpers/authenticator.js
@@ -25,7 +25,42 @@ function authenticator(settings) {
                     });
                     return;
                 }
-            }
+            } // REACT_NATIVE_LOGIN
+            else if (routeToCheck == '/login_api'){
+                console.log('usao u api login rutu');
+                console.log(req.body);
+                if (!validateEmail(req.body.unmLogin)) {
+                  var errorJSON = {
+                    status: 'ERROR',
+                  }
+                    res.json(errorJSON);
+                    return;
+                }
+                else {
+                    const hash = crypto.createHmac('sha256', settings.hashSecret).update(req.body.passLogin).digest('hex');
+                    pool.query("Select * from player where username=$1" , [req.body.unmLogin], function (err, result) {
+                        if (result.rows[0].password == hash) {
+                            var tokenJSON = {
+                              status: 'OK',
+                              token: util.cipherCookie(req.body.unmLogin)
+                            }
+                              console.log('trebalo bi raditi')
+                              res.json(tokenJSON);
+
+
+                            //res.cookie('authCookie', util.cipherCookie(req.body.unmLogin));
+                            //res.redirect('/playermap');
+                        }
+                        else {
+                            var errorJSON = {
+                              status: 'ERROR',
+                            }
+                            res.json(errorJSON);
+                        }
+                    });
+                    return;
+                }
+            } // END OF REACT_NATIVE_LOGIN
             else if (routeToCheck == '/register'){
                 if(!validateEmail(req.body.unmRegister)){
                     res.sendStatus(400);
@@ -50,24 +85,51 @@ function authenticator(settings) {
                     });
                 }
                 return;
+
             }
-            if (!req.cookies.authCookie) {
-                res.sendStatus(403);
-                return;
-            }
-            var token = util.decipherCookie(req.cookies.authCookie);
-            if (moment().isAfter(token.valid)) {
-                res.sendStatus(403);
-                return;
-            }
-            pool.query('Update player set isonline=true where username=$1 returning username', [token.username], function (err, result) {
-                if (result.rows[0].length == 0)
-                    res.sendStatus(403);
-                else {
-                    req.authUser = result.rows[0].username;
-                    next();
+            // REACT_NATIVE
+            if (req.query.rntoken){
+                ///////
+                console.log('usao u provjeru ');
+                var token = util.decipherCookie(req.query.rntoken);
+                if (moment().isAfter(token.valid)) {
+                    res.json({ status: 'ERROR'});
+
+                    return;
                 }
-            });
+                pool.query('Update player set isonline=true where username=$1 returning username', [token.username], function (err, result) {
+                    if (result.rows[0].length == 0)
+
+                        res.json({status: 'ERROR'});
+                    else {
+                        console.log('uspio?=?');
+                        req.authUser = result.rows[0].username;
+                        next();
+                    }
+                });
+
+                ///////
+            }
+            else if (!req.cookies.authCookie) {
+                res.sendStatus(403);
+                return;
+            } else {
+              // obicni
+              var token = util.decipherCookie(req.cookies.authCookie);
+              if (moment().isAfter(token.valid)) {
+                  res.sendStatus(403);
+                  return;
+              }
+              pool.query('Update player set isonline=true where username=$1 returning username', [token.username], function (err, result) {
+                  if (result.rows[0].length == 0)
+                      res.sendStatus(403);
+                  else {
+                      req.authUser = result.rows[0].username;
+                      next();
+                  }
+              });
+            }
+
         }
         else
             next();
